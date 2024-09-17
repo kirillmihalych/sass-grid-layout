316bd03ad77adfb42484f513d1996c5355695c2c
kirillmihalych <tiltlessman@gmail.com>
2024-09-17 13:29:21 +0300

    zooming images & expanding panels

---
+++ src/views/ExpandingPanels.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="panels">
+    <img src="../assets/коа_арт_пнг/аедирн_ком.png" alt="аедирн" />
+    <img src="../assets/коа_арт_пнг/атре_ком.png" alt="аттре" />
+    <img src="../assets/коа_арт_пнг/бремервоорд_ком.png" alt="бреммервоорд" />
+    <img src="../assets/коа_арт_пнг/бругге_ком.png" alt="бругге" />
+    <img src="../assets/коа_арт_пнг/верден_ком.png" alt="верден" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.panels {
+  display: grid;
+  width: calc(100vw - 2rem);
+  border: 5px solid green;
+  grid-template-columns: repeat(5, auto);
+  /* aspect-ratio: 1; */
+}
+
+.panels > img {
+  cursor: pointer;
+  transition: 0.5s linear;
+  object-fit: contain;
+  border: 2px solid black;
+  width: 0;
+  height: 350px;
+  min-height: 100%;
+  min-width: 100%;
+}
+
+.panels > img:hover {
+  width: 350px;
+}
+</style>
---
+++ src/views/ZoomImagesGrid.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="grid-images">
+    <img src="../assets/коа_арт_пнг/аедирн_ком.png" alt="аедирн" />
+    <img src="../assets/коа_арт_пнг/атре_ком.png" alt="аттре" />
+    <img src="../assets/коа_арт_пнг/бремервоорд_ком.png" alt="бреммервоорд" />
+    <img src="../assets/коа_арт_пнг/бругге_ком.png" alt="бругге" />
+    <img src="../assets/коа_арт_пнг/верден_ком.png" alt="верден" />
+    <img src="../assets/коа_арт_пнг/каедвен_ком.png" alt="каедвен" />
+    <img src="../assets/коа_арт_пнг/нильфгаард_ком.png" alt="нильфгаард" />
+    <img src="../assets/коа_арт_пнг/новиград_ком.png" alt="новиград" />
+    <img src="../assets/коа_арт_пнг/редания_ком.png" alt="редания" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped langs="scss">
+.grid-images {
+  --gap: 1rem;
+  --size: 350px;
+  --scale: 1.5;
+  width: calc((var(--gap) * 3) + (var(--size) * 2));
+  aspect-ratio: 1;
+  margin-block: 3rem;
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  gap: var(--gap);
+}
+
+.grid-images > img {
+  object-fit: contain;
+  width: 0;
+  height: 0;
+  min-width: 100%;
+  min-height: 100%;
+
+  border: 2px solid #313131;
+  border-radius: 2rem;
+  transition: 0.35s linear;
+}
+
+.grid-images > img:hover {
+  cursor: pointer;
+  width: calc(var(--size) * var(--scale));
+  height: calc(var(--size) * var(--scale));
+}
+</style>
